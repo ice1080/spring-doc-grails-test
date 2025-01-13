40f370e45c33f26a5b7e33d30d185b2e3662ddbb
Isaac Hudson <isaac.hudson@icd-tech.com>
2025-01-13 15:02:36 -0700

    changing javadocs to specific swagger annotations

--- grails-app/controllers/spring/doc/grails/TestController.groovy
+++ grails-app/controllers/spring/doc/grails/TestController.groovy
@@ -1,30 +1,37 @@
 package spring.doc.grails
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping('/api/test')
+@RequestMapping('api/test')
 @RestController
+@Tag(name = 'test-controller', description = 'Used for Test records')
 class TestController {
-
-    /** List all records */
+    @Operation(summary = 'List all records', description = "Returns a list of all records")
     @GetMapping
     def index() {
         render 'all records'
     }
 
-//    def show(@PathVariable() @Parameter(example = "123") String id) {
-
-    /** Show a single record */
+    @Operation(
+            summary = 'Retrieve a specific record',
+            description = "Returns a single record",
+            parameters = [
+                    @Parameter(name = 'id', description = 'The id of the record', example = '123')
+            ]
+    )
     @GetMapping("/{id}")
     def show() {
         render "showing record ${params.id}"
     }
 
-    /** Create a new record */
     @PostMapping
+    @Operation(summary = 'Create a new record', description = "Creates a new record and returns it in the response")
     def save() {
         render "record saved"
     }
