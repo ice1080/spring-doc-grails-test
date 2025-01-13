6f87923ca4a0466ef377b356351a330e72424811
Isaac Hudson <isaac.hudson@icd-tech.com>
2025-01-13 15:40:28 -0700

    getting PathVariable working for show

--- grails-app/controllers/spring/doc/grails/TestController.groovy
+++ grails-app/controllers/spring/doc/grails/TestController.groovy
@@ -2,16 +2,17 @@ package spring.doc.grails
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping('api/test')
 @RestController
 @Tag(name = 'test-controller', description = 'Used for Test records')
 class TestController {
+
     @Operation(summary = 'List all records', description = "Returns a list of all records")
     @GetMapping
     def index() {
@@ -19,15 +20,20 @@ class TestController {
     }
 
     @Operation(
-            summary = 'Retrieve a specific record',
+            summary = 'Retrieve a specific record by id',
             description = "Returns a single record",
             parameters = [
-                    @Parameter(name = 'id', description = 'The id of the record', example = '123')
+                    @Parameter(name = 'id', description = 'The id of the record', example = '456')
             ]
     )
-    @GetMapping("/{id}")
-    def show() {
-        render "showing record ${params.id}"
+    @ApiResponses(value = [
+            @ApiResponse(responseCode = '200', description = 'Record found'),
+            @ApiResponse(responseCode = '404', description = 'Record not found', content = @Content)
+    ])
+    @GetMapping(path = "/{id}")
+    // has to be protected for grails to skip it as an endpoint, in order for spring to register it without conflict
+    protected TestObject show(@PathVariable String id) {
+        new TestObject(id: id, name: "myName", other: 'otherStuff')
     }
 
     @PostMapping
---
+++ src/main/groovy/spring/doc/grails/TestObject.groovy
@@ -0,0 +1,12 @@
+package spring.doc.grails
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+class TestObject implements Serializable {
+    @Schema(description = 'The id of the object', example = '123', requiredMode = Schema.RequiredMode.REQUIRED)
+    String id
+    @Schema(description = 'The name of the object', example = 'myName', requiredMode = Schema.RequiredMode.REQUIRED)
+    String name
+    @Schema(description = 'The other of the object', example = 'otherStuff', requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    String other
+}
