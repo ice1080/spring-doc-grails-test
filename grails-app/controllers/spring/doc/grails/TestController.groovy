4839c4f5760d35cb308a549fbea5d55379755944
Isaac Hudson <isaac.hudson@icd-tech.com>
2025-01-13 12:08:56 -0700

    adding in test controller, servlet context, and springdoc

---
+++ grails-app/controllers/spring/doc/grails/TestController.groovy
@@ -0,0 +1,34 @@
+package spring.doc.grails
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RequestMapping('/api/test')
+@RestController
+class TestController {
+    static allowedMethods = [index: 'GET', show: 'GET', save: 'POST']
+
+    /** List all records */
+    @GetMapping
+    def index() {
+        render 'all records'
+    }
+
+//    def show(@PathVariable() @Parameter(example = "123") String id) {
+
+    /** Show a single record */
+    @GetMapping("/{id}")
+    def show() {
+        render "showing record ${params.id}"
+    }
+
+    /** Create a new record */
+    @PostMapping
+    def save() {
+        render "record saved"
+    }
+
+}
